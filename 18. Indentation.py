f0f60dc5aeb7862f285805bfb607833012de90ba
himanshugehlot280 <87193425+himanshugehlot280@users.noreply.github.com>
2022-12-27 19:18:43 +0530

    Add files via upload

---
+++ 18. Indentation.py
@@ -0,0 +1,19 @@
+"""
+Indentation refers to the spaces at the beginning of a code line.
+Python uses indentation to indicate a block of code
+python will give you error if you skip the indentation
+the number of spaces is up to you as a programmer. but it has to be at least one
+you have to use the same number of spaces in same block of code, otherwise
+python will give you an error
+
+below there are two programs which elaborate the need of indentation in python program
+
+"""
+# ----------------1)----------------------- no error code
+if 5 > 2:
+    print('no error')
+
+# ---------------2)------------------------error code
+# if 5>2:
+# print('error')
+# print('above code consist error ')
