b3dc6be499250a633e78f125720e0de2119f8b8a
Himanshu Gehlot <87193425+himanshugehlot280@users.noreply.github.com>
2022-12-28 23:34:13 +0530

    Add files via upload

---
+++ 28. end parameter use .py
@@ -0,0 +1,18 @@
+# Python end parameter
+"""
+The print() function in python always creates a new line.
+but there is alsa a parameter for this function which can put other characters instead of new line
+at the end which is called end parameter """
+
+print('Welcome')
+print('Welcome')
+print('Welcome')
+print('-------------------------------------------')
+print('Hello',end='')
+print('Hello')
+print('Hello',end=' ')
+print('Hello')
+print('Hello',end=' and ')
+print('Hello')
+print('Hello',end='\n')
+print('Hello')
