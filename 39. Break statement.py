cdb55135b92869764906969d69b1dfb45cf6fd99
Himanshu Gehlot <87193425+himanshugehlot280@users.noreply.github.com>
2023-03-09 14:49:54 +0530

    Add files via upload

---
+++ 39. Break statement.py
@@ -0,0 +1,12 @@
+for i in range(1,11):
+    if i == 5:
+        break
+    print(i,end=' ')
+print('')
+print('loop is finished')
+for i in range(1,11):
+    if i == 7 or i == 5:
+        break
+    print(i,end=' ')
+print('')
+print('loop is finished')
